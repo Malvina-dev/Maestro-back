e827cf18aa831adcee6cd22f12fb73859c7e6921
SoleneSol <so.buono@gmail.com>
2025-10-16 11:58:43 +0200

    commentaires pour les associations OK

--- app/models/index.js
+++ app/models/index.js
@@ -1,20 +1,22 @@
 import Preview from "./previewModel.js";
 import Genre from "./genreModel.js";
 
+// DEFINITION DE LA TABLE DE LIAISON ENTRE LES TABLES GENRE ET PREVIEW
+
 // une Preview peut avoir plusieurs Genres
 Preview.belongsToMany(Genre, {
-	through: "preview_genre",
-	foreignKey: "preview_id",
-    otherKey: "genre_id",
-    as: "genre"
+	through: "preview_genre", // nom de la table de liaison
+	foreignKey: "preview_id", // comment la clé de Preview sera écrite sur la table de liaison
+    otherKey: "genre_id", // other key fait référence à la clé de l'autre table (ici de la table genre)
+    as: "listGenres" // l'alias qu'on donne pour récupérer l'ensemble des genres d'une preview ex: preview.listGenres
 });
 
 // un Genre peut avoir plusieurs Previews
 Genre.belongsToMany(Preview, {
-    through: "preview_genre",
-    foreignKey: "genre_id",
-    otherKey: "preview_id",
-    as: "preview"
+    through: "preview_genre", // nom de la table de liaison
+    foreignKey: "genre_id", // comment la clé de Genre sera écrite sur la table de liaison
+    otherKey: "preview_id", // other key fait référence à la clé de l'autre table (ici de la table preview)
+    as: "listPreviews" // l'alias qu'on donne pour récupérer l'ensemble des previews d'un genre ex: genre.listPreviews
 });
 
 export { Preview, Genre };
