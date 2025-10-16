472ea3c837de59ad4bcc5a53dd19a2658439c003
SoleneSol <so.buono@gmail.com>
2025-10-16 11:48:34 +0200

    tables Genre et Preview, commentaire

--- app/models/index.js
+++ app/models/index.js
@@ -1,14 +1,15 @@
 import Preview from "./previewModel.js";
 import Genre from "./genreModel.js";
 
-// une preview peut avoir plusieurs genres
+// une Preview peut avoir plusieurs Genres
 Preview.belongsToMany(Genre, {
-	through: "preview_genre", // preview a un ou des genres
-	foreignKey: "preview_id", // dans la future table foreign key
+	through: "preview_genre",
+	foreignKey: "preview_id",
     otherKey: "genre_id",
     as: "genre"
 });
 
+// un Genre peut avoir plusieurs Previews
 Genre.belongsToMany(Preview, {
     through: "preview_genre",
     foreignKey: "genre_id",
