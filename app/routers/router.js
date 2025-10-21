248c17d6210392b4de59ef85b113829c5008eb9e
Anthonynoel22 <anthony.noel-kiruparajah@oclock.school>
2025-10-21 14:36:23 +0200

    router + comtroller + test pour projet

--- app/routers/router.js
+++ app/routers/router.js
@@ -1,21 +1,19 @@
-import express from "express";
-import companyRoute from "./companyRoute.js";
+
 import userRoute from "./userRoute.js";
 import express from 'express';
 import companyRoute from './companyRoute.js';
 import messageContactRoute from './messageContactRoute.js';
 import previewRoute from './previewRoute.js';
 import genreRoute from './genreRouter.js';
+import projectRouter from "./projectRouter.js";
 
 const router = express.Router();
 
-router.use("/api", companyRoute);
-router.use("/api", userRoute);
 
-export default router;
 router.use('/api', companyRoute);
 router.use('/api', messageContactRoute);
 router.use('/api', previewRoute);
 router.use('/api', genreRoute );
+router.use('/api', projectRouter);
 
 export default router;
