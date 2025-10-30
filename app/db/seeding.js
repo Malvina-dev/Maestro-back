1e19bdbe4718a8eb0429e8edfbb701095d3c28b2
Malvina <malvina.gilardeau@gmail.com>
2025-10-30 14:18:32 +0100

    refactor : adding seeding project

--- app/db/seeding.js
+++ app/db/seeding.js
@@ -6,7 +6,7 @@ import {
     Preview,
     Genre,
     MessageContact,
-    Description
+    Description,
 } from "../models/index.js";
 import bcrypt from "bcryptjs";
 
@@ -33,36 +33,34 @@ try {
         siret: "42345678912345",
         localisation: "Bordeaux",
     });
-    
-
 
     await Preview.create({
         title: "Extrait 1",
         isStar: false,
-        date: new Date("2025-01-01")
+        date: new Date("2025-01-01"),
     }),
-    await Preview.create({
-        title: "Extrait 2",
-        isStar: false,
-        date: new Date("2025-03-03")
-    }),
-    await Preview.create({
-        title: "Extrait 3",
-        isStar: true,
-        date: new Date("2025-05-05")
-    }),
-    // await Preview.create({
-    //     title: "Extrait à supprimer",
-    //     isStar: true,
-    //     date: new Date("2025-06-06"),
-    //     link: "uploads/RAYE - WHERE IS MY HUSBAND! (Official Music Video).mp3-1761222883667-884693652"
-    // })
+        await Preview.create({
+            title: "Extrait 2",
+            isStar: false,
+            date: new Date("2025-03-03"),
+        }),
+        await Preview.create({
+            title: "Extrait 3",
+            isStar: true,
+            date: new Date("2025-05-05"),
+        }),
+        // await Preview.create({
+        //     title: "Extrait à supprimer",
+        //     isStar: true,
+        //     date: new Date("2025-06-06"),
+        //     link: "uploads/RAYE - WHERE IS MY HUSBAND! (Official Music Video).mp3-1761222883667-884693652"
+        // })
 
-    // messages-contact
-    await MessageContact.create({
-        mail: "test1@test.com",
-        message: "Maestro is magic",
-    });
+        // messages-contact
+        await MessageContact.create({
+            mail: "test1@test.com",
+            message: "Maestro is magic",
+        });
     await MessageContact.create({
         mail: "test2@test.com",
         message: "Maestro les meilleurs",
@@ -77,68 +75,85 @@ try {
     const passwordHashed1 = await bcrypt.hash("gargouille2026", 10);
     await User.create({
         email: "robin.syntaxe@exemple.com",
-        password: passwordHashed1
+        password: passwordHashed1,
     });
 
     const passwordHashed2 = await bcrypt.hash("gargouille2027", 10);
     await User.create({
         email: "camille.script@exemple.com",
-        password: passwordHashed2
+        password: passwordHashed2,
     });
 
     const passwordHashed3 = await bcrypt.hash("gargouille2028", 10);
     await User.create({
         email: "alex.flexbox@exemple.com",
-        password: passwordHashed3
+        password: passwordHashed3,
     });
 
     const passwordHashed4 = await bcrypt.hash("gargouille2029", 10);
     await User.create({
         email: "zoé.pixel@exemple.com",
-        password: passwordHashed4
+        password: passwordHashed4,
     });
-
 
     // Description-portfolio
     await Description.create({
-        title: 'Title1',
-        image_link: 'Image1',
-        text: 'Text1'
+        title: "Title1",
+        image_link: "Image1",
+        text: "Text1",
     });
     await Description.create({
-        title: 'Title2',
-        image_link: 'Image2',
-        text: 'Text1'
+        title: "Title2",
+        image_link: "Image2",
+        text: "Text1",
     });
     await Description.create({
-        title: 'Title3',
-        image_link: 'Image3',
-        text: 'Text3'
+        title: "Title3",
+        image_link: "Image3",
+        text: "Text3",
     });
 
+    await Genre.create({
+        label: "classique",
+    });
+    await Genre.create({
+        label: "rock",
+    });
+    await Genre.create({
+        label: "jazz",
+    });
 
-    await Genre.create({
-        label: "classique"
+    // Project
+    await Projet.create({
+        name: "Title 1",
+        resume: "Resume 1",
+        status: "en cours",
+        deadline: new Date("2025-05-05"),
     });
-    await Genre.create({
-        label: "rock"
+    await Projet.create({
+        name: "Title 2",
+        resume: "Resume 2",
+        status: "à commencer",
+        deadline: new Date("2025-05-05"),
     });
-    await Genre.create({
-        label: "jazz"
+    await Projet.create({
+        name: "Title 3",
+        resume: "Resume 3",
+        status: "terminé",
+        deadline: new Date("2025-05-05"),
     });
-
+    
     const users = await User.findAll();
     const companies = await Company.findAll();
     const genres = await Genre.findAll();
     const previews = await Preview.findAll();
-  
+
     await companies[0].addListUsers([users[0]]);
     await companies[1].addListUsers([users[1]]);
-  
+
     await previews[0].addListGenres([genres[0], genres[2]]);
     await previews[1].addListGenres([genres[1], genres[2]]);
     await previews[2].addListGenres([genres[0]]);
-
 } catch (error) {
     console.error(error);
 }
